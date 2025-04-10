bb6f54431ea4e14f0686d0ed76c27e3695728983
NikolayPutyata <93452811+NikolayPutyata@users.noreply.github.com>
2025-04-10 23:21:35 +0300

    upd

--- src/components/StarEater.jsx
+++ src/components/StarEater.jsx
@@ -131,8 +131,8 @@ const StarEater = () => {
           coin.x = Math.random() * (width - 20) + 10;
           coin.y = Math.random() * (height - 20) + 10;
           coin.anchor.set(0.5); // Центрируем монету
-          coin.width = Math.max(10, width * 0.6); // Устанавливаем размер
-          coin.height = Math.max(10, width * 0.6);
+          coin.width = Math.max(10, width * 0.06); // Устанавливаем размер
+          coin.height = Math.max(10, width * 0.06);
           app.stage.addChild(coin);
           coins.push(coin);
 
